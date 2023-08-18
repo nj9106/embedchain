692b4c8032a4933a1d342f6837065507b6c4490c
Liang <nj9106@gmail.com>
2023-08-18 13:36:14 +0800

    fix small issues

--- add_file.py
+++ add_file.py
@@ -1,7 +1,7 @@
 import re
 import os
 import sys
-import json
+import json5
 from embedchain import App
 from embedchain.config import AppConfig
 
@@ -9,7 +9,6 @@ from embedchain.config import AppConfig
 appConfig = AppConfig(log_level='DEBUG')
 qa = App(config=appConfig)
 meta = {"id":"1131690354001020772","summary":"this is an example summary."}
-qa.add_local("text", "this is an example question.", metadata=meta)
 filename = '/root/tapd/tapd.txt'
 i = 0
 with open(filename, 'r') as file:
@@ -17,6 +16,8 @@ with open(filename, 'r') as file:
     for line in lines:
         i = i +1
         print(f"Processing {i} record ... ...")
-        q = json.loads(line)
+        q = json5.loads(line)
         # {'id': '1131690354001021028', 'desc': '', 'summary': ''}
-        qa.add_local("text", q['desc'], metadata={"id":q['id'],"summary":q['summary']})
+        meta = {"id":q['id'],"summary":q['summary']}
+        print(meta)
+        qa.add_local("text", q['desc'], metadata=meta)
--- query.py
+++ query.py
@@ -1,11 +1,16 @@
 import os
+import sys
 
 from embedchain import App
 from embedchain.config import AppConfig
 from embedchain.config import QueryConfig
 
+if len(sys.argv) < 2:
+    print("没有提供问题")
+    sys.exit(0)
+
 appConfig = AppConfig(log_level='DEBUG')
 qa = App(config=appConfig)
-q = "this is an example question."
+q = sys.argv[1]
 queryConfig = QueryConfig(number_documents=3)
 print(qa.retrieve_from_database(q, config=queryConfig))
