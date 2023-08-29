73501449465c6719f7e4b2899108a895164540d4
Liang <nj9106@gmail.com>
2023-08-29 15:33:32 +0800

    add input parar

--- add_file.py
+++ add_file.py
@@ -5,11 +5,14 @@ import json5
 from embedchain import App
 from embedchain.config import AppConfig
 
+if len(sys.argv) < 2:
+    print("没有文件路径")
+    sys.exit(0)
+filename = sys.argv[1]
 
 appConfig = AppConfig(log_level='DEBUG',collect_metrics=False)
 qa = App(config=appConfig)
 # meta = {"id":"1131690354001020772","summary":"this is an example summary."}
-filename = '/root/tapd/tapd.txt'
 i = 0
 with open(filename, 'r') as file:
     lines = file.readlines()
---
+++ api_server.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, request
+
+from embedchain import App
+from embedchain.config import AppConfig
+from embedchain.config import QueryConfig
+
+app = Flask(__name__)
+
+appConfig = AppConfig(log_level='DEBUG',collect_metrics=False)
+queryConfig = QueryConfig(number_documents=3)
+urlPrefix = 'https://www.tapd.cn/31690354/bugtrace/bugs/view?bug_id='
+
+knowledgeConfig = AppConfig(collection_name='knowledge',log_level='DEBUG',collect_metrics=False)
+knowledgePrefix  = 'https://helpy-dev.plaso.cn/zh/'
+kn_queryConfig = QueryConfig(number_documents=1)
+
+def initialize_chat_bot():
+    global chat_bot
+    global knowledge
+    chat_bot = App(config=appConfig)
+    knowledge = App(config=knowledgeConfig)
+
+# @app.route("/add", methods=["POST"])
+# def add():
+#     data = request.get_json()
+#     data_type = data.get("data_type")
+#     url_or_text = data.get("url_or_text")
+#     if data_type and url_or_text:
+#         try:
+#             chat_bot.add(data_type, url_or_text)
+#             return jsonify({"data": f"Added {data_type}: {url_or_text}"}), 200
+#         except Exception:
+#             return jsonify({"error": f"Failed to add {data_type}: {url_or_text}"}), 500
+#     return jsonify({"error": "Invalid request. Please provide 'data_type' and 'url_or_text' in JSON format."}), 400
+
+
+@app.route("/query", methods=["POST"])
+def query():
+    data = request.get_json()
+    question = data.get("question")
+    if question:
+        try:
+            results = chat_bot.retrieve_from_database(question, config=queryConfig)
+            out = []
+            i = 0
+            while i < len(results['documents'][0]):
+                metadata = results['metadatas'][0][i]
+                o = {"question": results['documents'][0][i], "summary": metadata['summary'], "url": f"{urlPrefix}{metadata['id']}"}
+                out.append(o)
+                i = i+1
+            results = knowledge.retrieve_from_database(question,kn_queryConfig)
+            i = 0
+            while i < len(results['documents'][0]):
+                metadata = results['metadatas'][0][i]
+                o = {"question": results['documents'][0][i], "summary": metadata['summary'], "url": f"{knowledgePrefix}{metadata['path']}"}
+                out.append(o)
+                i = i+1
+            return jsonify(out), 200
+        except Exception as e:
+            print(e)
+            return jsonify({"error": "An error occurred. Please try again!"}), 500
+    return jsonify({"error": "Invalid request. Please provide 'question' in JSON format."}), 400
+
+
+# @app.route("/chat", methods=["POST"])
+# def chat():
+#     data = request.get_json()
+#     question = data.get("question")
+#     if question:
+#         try:
+#             response = chat_bot.chat(question)
+#             return jsonify({"data": response}), 200
+#         except Exception:
+#             return jsonify({"error": "An error occurred. Please try again!"}), 500
+#     return jsonify({"error": "Invalid request. Please provide 'question' in JSON format."}), 400
+
+
+if __name__ == "__main__":
+    initialize_chat_bot()
+    app.run(host="0.0.0.0", port=5001, debug=False)
