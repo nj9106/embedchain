968f741b3e073635294b0d79d3cd541350073ca5
Liang <nj9106@gmail.com>
2023-08-30 11:13:57 +0800

    modify add_new.sh

--- add_new.sh
+++ add_new.sh
@@ -1,8 +1,9 @@
 cd /root/embedchain
 mkdir -p ../bak_tapd
-yesterday=$(date -d "yesterday" +"%Y-%m-%d")
 today=$(date +"%Y-%m-%d")
+yesterday=$(date -d "yesterday" +"%Y-%m-%d")
+twoDaysAgo=$(date -d "2 days ago" +"%Y-%m-%d")
 echo ${today} >> add_new.log 2>&1
-mv ./tapd.txt ../bak_tapd/tapd${yesterday}.txt
-python3 tapd.py ${today} >> add_new.log 2>&1
+mv ./tapd.txt ../bak_tapd/tapd${twoDaysAgo}.txt
+python3 tapd.py ${yesterday} >> add_new.log 2>&1
 python3 add_file.py ./tapd.txt >> add_new.log 2>&1
